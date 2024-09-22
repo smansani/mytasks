964b0e12908519cafed54835ecf619bd86b0820c
smansani <94820506+smansani@users.noreply.github.com>
2024-09-23 01:24:24 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -12,7 +12,7 @@ export default function Signup() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        const response = await fetch("/api/auth/createuser", {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
